d88b6feefd22c8b80d57ca44e8c62bc27c0573a9
Javier Marquez <jamarquez145@gmail.com>
2021-07-27 16:11:12 -0400

    Commit con proyecto, sin pruebas unitarias.

---
+++ src/main/java/contrasenia/GeneraContrasenia.java
@@ -0,0 +1,32 @@
+package contrasenia;
+
+import java.util.Random;
+
+public class GeneraContrasenia {
+
+    private static Character seleccionarCaracter() {
+        String numeros = "0123456789";
+        String minusculas = "abcdefghijklmnñopqrstuvwxyz";
+        String mayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+        String simbolos = "!#@%&/?¿-_:.;,<>=´*+÷";
+
+        char seleccionado = 0;
+
+        switch ((int) (Math.random() * 4)) {
+            case 0 -> seleccionado = numeros.charAt((int) (Math.random() * numeros.length()));
+            case 1 -> seleccionado = mayusculas.charAt((int) (Math.random() * mayusculas.length()));
+            case 2 -> seleccionado = minusculas.charAt((int) (Math.random() * minusculas.length()));
+            case 3 -> seleccionado = simbolos.charAt((int) (Math.random() * simbolos.length()));
+        }
+        return seleccionado;
+    }
+    public static String generarContrasenia() {
+
+        String contrasenia = "";
+
+        do {
+            contrasenia += seleccionarCaracter();
+        } while (!AnalizaContrasenia.verificarContrasenia(contrasenia).equals("SEGURA"));
+        return contrasenia;
+    }
+}
---
+++ src/main/java/lanzador/App.java
@@ -0,0 +1,12 @@
+package lanzador;
+
+import contrasenia.AnalizaContrasenia;
+import contrasenia.GeneraContrasenia;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println(GeneraContrasenia.generarContrasenia());
+    }
+}
